b645fbf88687de1079b3a3770394628ed52d2db2
allen9535 <allen9535@naver.com>
2023-08-18 16:33:25 +0900

    feat(carts): view 생성

--- carts/models.py
+++ carts/models.py
@@ -7,6 +7,6 @@ User = get_user_model()
 
 class Cart(models.Model):
     product     =   models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="상품")
-    buyer       =   models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="구매자")
+    user       =   models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="구매자")
     amount      =   models.IntegerField(default=0, verbose_name="수량")
     created_at  =   models.DateTimeField(auto_now_add=True, verbose_name="생성일")
--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .models import Cart
+from .serializers import CartSerializer
+
+
 # Create your views here.
+class CartView(APIView):
+    def get(self, request):
+        cart = Cart.objects.filter(user=request.user)
+        serializer = CartSerializer(cart, many=True)
+
+        if serializer:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = CartSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        cart = Cart.objects.filter(user=request.user).filter(request.data["cart_id"])
+        serializer = CartSerializer(cart, data=request.data["amount"], partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
