12dc614096d252b736167d5ff445b5bcf62af0eb
allen9535 <allen9535@naver.com>
2023-08-18 17:03:37 +0900

    fix(carts): views 수정

--- carts/views.py
+++ carts/views.py
@@ -12,12 +12,16 @@ from .serializers import CartSerializer
 class CartView(APIView):
     def get(self, request):
         cart = Cart.objects.filter(user=request.user)
+        
+        if not cart:
+            return Response({"message": "장바구니가 비었습니다."}, status=status.HTTP_404_NOT_FOUND)
+        
         serializer = CartSerializer(cart, many=True)
 
         if serializer:
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({"message": "오류가 발생했습니다."}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request):
         serializer = CartSerializer(data=request.data)
@@ -25,14 +29,15 @@ class CartView(APIView):
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"message": "잘못된 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request):
-        cart = Cart.objects.filter(user=request.user).filter(request.data["cart_id"])
-        serializer = CartSerializer(cart, data=request.data["amount"], partial=True)
+        cart = Cart.objects.filter(user=request.user, id=request.data["cart_id"]).first()
+        serializer = CartSerializer(cart, data={"amount": request.data["amount"]}, partial=True)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "잘못된 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
