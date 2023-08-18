29fe8abf48152c5a4df3a5c9b43248396ed2b765
allen9535 <allen9535@naver.com>
2023-08-18 17:17:18 +0900

    fix(carts): models 코드 컨벤션 수정

--- carts/models.py
+++ carts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Cart(models.Model):
-    product     =   models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="상품")
-    user       =   models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="구매자")
-    amount      =   models.IntegerField(default=0, verbose_name="수량")
-    created_at  =   models.DateTimeField(auto_now_add=True, verbose_name="생성일")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="상품")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="구매자")
+    amount = models.IntegerField(default=0, verbose_name="수량")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
