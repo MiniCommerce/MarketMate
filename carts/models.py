11fc1f69bd074f6d26e07363cff16602045282b8
allen9535 <allen9535@naver.com>
2023-08-18 15:27:44 +0900

    feat(carts): 장바구니 모델 생성

---
+++ carts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+User = get_user_model()
+
+class Cart(models.Model):
+    product     =   models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="상품")
+    buyer       =   models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="구매자")
+    amount      =   models.IntegerField(default=0, verbose_name="수량")
+    created_at  =   models.DateTimeField(auto_now_add=True, verbose_name="생성일")
