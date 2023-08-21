2e93197a44db05081e79f15fccdf45e401ba01bf
taencoding <xodms512@gmail.com>
2023-08-21 13:35:56 +0900

    feat(reviews): view, url 작성

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-
+    path("list/", views.ReviewList.as_view()),
+    path("write/", views.ReviewList.as_view()),
+    path("update/", views.ReviewDetail.as_view()),
+    path("delete/", views.ReviewDetail.as_view()),
 ]
