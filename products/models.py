329c00dd6e267a01e11893e78ff4820539639255
allen9535 <allen9535@naver.com>
2023-08-22 09:51:55 +0900

    fix(products): 모델 수정

--- products/models.py
+++ products/models.py
@@ -17,23 +17,21 @@ class Category(models.Model):
     name = models.CharField(blank=False, null=False, verbose_name='카테고리명')
 
 
-class ProductImage(models.Model):
-    image = models.ImageField(upload_to=image_path, verbose_name='상품 이미지')
-
-
 class Product(models.Model):
-
-
-    seller = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, verbose_name='판매자')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='카테고리')
-    product_name = models.CharField(blank=False, null=False, max_length=200, verbose_name='상품명')
-    price = models.IntegerField(blank=False, null=False, verbose_name='가격')
-    amount = models.IntegerField(default=0, blank=False, null=False, verbose_name='수량')
+    seller = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, verbose_name="판매자")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="카테고리")
+    product_name = models.CharField(blank=False, null=False, max_length=200, verbose_name="상품명")
+    price = models.IntegerField(blank=False, null=False, verbose_name="가격")
+    amount = models.IntegerField(default=0, blank=False, null=False, verbose_name="수량")
     desc = models.TextField()
-    score = models.FloatField(default=0, verbose_name='평점')
+    score = models.FloatField(default=0, verbose_name="평점")
     thumbnail_image = models.ImageField(upload_to=image_path, blank=True, null=True, verbose_name="썸네일")
-    image = models.ForeignKey(ProductImage, blank=True, null=True, on_delete=models.CASCADE, verbose_name="상품 이미지")
-    status = models.CharField(max_length=30, verbose_name='상태')
-    order_status = models.CharField(max_length=30, verbose_name='주문 상태')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='등록일')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
+    status = models.CharField(max_length=30, verbose_name="상태")
+    order_status = models.CharField(max_length=30, verbose_name="주문 상태")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="등록일")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, blank=False, null=False, on_delete=models.CASCADE, verbose_name="상품")
+    image = models.ImageField(upload_to=image_path, verbose_name="상품 이미지")
