02cb306372b1a89e4e0d161af8674e1fbd02ee7a
taencoding <xodms512@gmail.com>
2023-08-22 09:22:35 +0900

    chore(reviews): review 제외 앱 삭제

--- reviews/models.py
+++ reviews/models.py
@@ -10,6 +10,6 @@ User = get_user_model()
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="상품")
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="리뷰작성자")
-    desc = models.CharField(max_length=200, verbose_name="상세설명")
+    desc = models.CharField(max_length=200, verbose_name="리뷰내용")
     score = models.FloatField(min_value=0, max_value=5, verbose_name="평점")
     created_at = models.DateTimeField(auto_now_add=True)
