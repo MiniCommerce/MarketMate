b5e93429a2f90db5cabad7208b4e1e98c3efb30e
allen9535 <allen9535@naver.com>
2023-08-22 09:34:08 +0900

    feat(products): 상품 모델 생성

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from main.env import config
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,8 +46,6 @@ INSTALLED_APPS = [
     'rest_auth',
 
     'main',
-    'users',
-    'carts',
     'products',
 ]
 
@@ -125,6 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join('staticfiles')
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
