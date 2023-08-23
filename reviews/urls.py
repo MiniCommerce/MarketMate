8188d969154e7954a607079278390151edb6cf76
taencoding <xodms512@gmail.com>
2023-08-23 10:47:06 +0900

    feat(reviews): form data 전달 방식으로 변경

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    path("list/<int:product_id>/", views.ReviewList.as_view()),
-    path("write/<int:product_id>/", views.ReviewList.as_view()),
-    path("update/<int:review_id>/", views.ReviewDetail.as_view()),
-    path("delete/<int:review_id>/", views.ReviewDetail.as_view()),
+    path("list/", views.ReviewList.as_view()),
+    path("write/", views.ReviewList.as_view()),
+    path("update/", views.ReviewDetail.as_view()),
+    path("delete/", views.ReviewDetail.as_view()),
 ]
