886d484f944dea5b3dc201e3d471b6b45925e149
allen9535 <allen9535@naver.com>
2023-08-29 22:24:57 +0900

    fix(rds): 배포를 위한 마이너 수정

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
