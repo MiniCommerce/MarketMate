d5a62da4d4f9be953a46988e31cdbdddca955d5e
allen9535 <allen9535@naver.com>
2023-09-03 15:09:41 +0900

    fix(main): 개발 사양으로 세팅 수정

--- main/settings.py
+++ main/settings.py
@@ -29,11 +29,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com",
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,7 +70,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://marketmate-fe.s3-website.ap-northeast-2.amazonaws.com',
     'localhost',
     '127.0.0.1'
 ]
@@ -107,12 +102,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default=None),
-        'USER': config('DB_USER', default=None),
-        'PASSWORD': config('DB_PASSWORD', default=None),
-        'HOST': config('DB_HOST', default=None),
-        'PORT': config('DB_PORT', default=None)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
